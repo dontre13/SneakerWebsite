2202a77e0e86fc769e2b47b5f8d1d1c502614b80
Dontre <d.waggoner85@gmail.com>
2025-06-02 21:22:15 -0700

    first commit

---
+++ js/js.js
@@ -0,0 +1,328 @@
+const products = [
+    {
+        id: 1,
+        title: "Jordan 1s",
+        price: "$250",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        colors: [
+            {
+                name: "Black/White/Red",
+                mainImage: "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-2.jpg",
+                    "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-3.jpg",
+                    "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-4.jpg"
+                ],
+                sizes: [9, 9.5, 10, 11.5, 12]
+            },
+            {
+                name: "Gold/White/Black",
+                mainImage: "../img/Jordan1/GoldWhiteBlack/Jordan-1s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan1/GoldWhiteBlack/Jordan-1s-image-2.jpg",
+                    "../img/Jordan1/GoldWhiteBlack/Jordan-1s-image-3.jpg",
+                    "../img/Jordan1/GoldWhiteBlack/Jordan-1s-image-4.jpg"
+                ],
+                sizes: [9, 10, 10.5, 11, 12]
+            },
+            {
+                name: "Grey/White/Black",
+                mainImage: "../img/Jordan1/GreyWhiteBlack/Jordan-1s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan1/GreyWhiteBlack/Jordan-1s-image-2.jpg",
+                    "../img/Jordan1/GreyWhiteBlack/Jordan-1s-image-3.jpg",
+                    "../img/Jordan1/GreyWhiteBlack/Jordan-1s-image-4.jpg"
+                ],
+                sizes: [9, 10, 10.5, 11, 12]
+            },
+            {
+                name: "White/Black",
+                mainImage: "../img/Jordan1/WhiteBlack/Jordan-1s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan1/WhiteBlack/Jordan-1s-image-2.jpg",
+                    "../img/Jordan1/WhiteBlack/Jordan-1s-image-3.jpg",
+                    "../img/Jordan1/WhiteBlack/Jordan-1s-image-4.jpg"
+                ],
+                sizes: [9, 10, 10.5, 11, 12]
+            }         
+        ]
+    },
+    {
+        id: 2,
+        title: "Jordan 2s",
+        price: "$350",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        colors: [
+            {
+                name: "LegendBlue/White",
+                mainImage: "../img/Jordan2/LegendBlueWhite/Jordan-2s-image-2.jpg",
+                thumbnails: [
+                    "../img/Jordan2/LegendBlueWhite/Jordan-2s-image-1.jpg",
+                    "../img/Jordan2/LegendBlueWhite/Jordan-2s-image-3.jpg",
+                    "../img/Jordan2/LegendBlueWhite/Jordan-2s-image-4.jpg",
+                    "../img/Jordan2/LegendBlueWhite/Jordan-2s-image-5.jpg"
+                ],
+        sizes: [9, 9.5, 10, 11.5, 12]
+            },
+            {
+                name: "Red/Black/Grey",
+                mainImage: "../img/Jordan2/RedBlackGrey/Jordan-2s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan2/RedBlackGrey/Jordan-2s-image-2.jpg",
+                    "../img/Jordan2/RedBlackGrey/Jordan-2s-image-3.jpg",
+                    "../img/Jordan2/RedBlackGrey/Jordan-2s-image-4.jpg",
+                    "../img/Jordan2/RedBlackGrey/Jordan-2s-image-5.jpg"
+                ],
+        sizes: [7, 8, 10, 11, 12.5]
+            },
+            {
+                name: "White/Black/MetallicGold",
+                mainImage: "../img/Jordan2/WhiteBlackMetalicGold/Jordan-2s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan2/WhiteBlackMetalicGold/Jordan-2s-image-2.jpg",
+                    "../img/Jordan2/WhiteBlackMetalicGold/Jordan-2s-image-3.jpg",
+                    "../img/Jordan2/WhiteBlackMetalicGold/Jordan-2s-image-4.jpg",
+                    "../img/Jordan2/WhiteBlackMetalicGold/Jordan-2s-image-5.jpg"
+                ],
+        sizes: [7.5, 8.5, 9, 10, 12.5]
+            }  
+        ]
+    },
+    {
+        id: 3,
+        title: "Jordan 1s Hightops",
+        price: "$300",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        colors: [
+            {
+                name: "Black/Silver/White",
+                mainImage: "../img/Jordan1_Hightop/BlackSilverWhite/Jordan_HighTops-1s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan1_Hightop/BlackSilverWhite/Jordan_HighTops-1s-image-2.jpg",
+                    "../img/Jordan1_Hightop/BlackSilverWhite/Jordan_HighTops-1s-image-3.jpg",
+                    "../img/Jordan1_Hightop/BlackSilverWhite/Jordan_HighTops-1s-image-4.jpg",
+                    "../img/Jordan1_Hightop/BlackSilverWhite/Jordan_HighTops-1s-image-5.jpg"
+                ],
+        sizes: [9, 9.5, 10, 11.5, 12]
+            },
+            {
+                name: "White/CarolinaBlue",
+                mainImage: "../img/Jordan1_Hightop/WhiteCarolinaBlue/Jordan_HighTops-1s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan1_Hightop/WhiteCarolinaBlue/Jordan_HighTops-1s-image-2.jpg",
+                    "../img/Jordan1_Hightop/WhiteCarolinaBlue/Jordan_HighTops-1s-image-3.jpg",
+                    "../img/Jordan1_Hightop/WhiteCarolinaBlue/Jordan_HighTops-1s-image-4.jpg",
+                    "../img/Jordan1_Hightop/WhiteCarolinaBlue/Jordan_HighTops-1s-image-5.jpg"
+                ],
+        sizes: [9, 9.5, 10, 11.5, 12]
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Jordan 4s",
+        price: "$375",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        colors: [
+            {
+                name: "White/Black",
+                mainImage: "../img/Jordan 4/WhiteBlack/Jordan-4s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan 4/WhiteBlack/Jordan-4s-image-2.jpg",
+                    "../img/Jordan 4/WhiteBlack/Jordan-4s-image-3.jpg",
+                    "../img/Jordan 4/WhiteBlack/Jordan-4s-image-4.jpg",
+                    "../img/Jordan 4/WhiteBlack/Jordan-4s-image-5.jpg"
+                ],
+        sizes: [9, 9.5, 10, 11.5, 12]
+            },
+            {
+                name: "White/Navy/WolfGrey",
+                mainImage: "../img/Jordan 4/WhiteBlack/Jordan-4s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan4/WhiteNavyWolfGrey/Jordan-4s-image-2.jpg",
+                    "../img/Jordan4/WhiteNavyWolfGrey/Jordan-4s-image-3.jpg",
+                    "../img/Jordan4/WhiteNavyWolfGrey/Jordan-4s-image-4.jpg",
+                    "../img/Jordan4/WhiteNavyWolfGrey/Jordan-4s-image-5.jpg"
+                ],
+        sizes: [9, 9.5, 10, 11.5, 12]
+            }
+            
+        ]
+    },
+    {
+        id: 5,
+        title: "Jordan 11s",
+        price: "$375",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        colors: [
+            {
+                name: "Black/Red",
+                mainImage: "../img/Jordan11/BlackRed/Jordan-11s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan 4/WhiteBlack/Jordan-4s-image-2.jpg",
+                    "../img/Jordan 4/WhiteBlack/Jordan-4s-image-3.jpg",
+                    "../img/Jordan 4/WhiteBlack/Jordan-4s-image-4.jpg",
+                    "../img/Jordan 4/WhiteBlack/Jordan-4s-image-5.jpg"
+                ],
+        sizes: [9, 9.5, 10, 11.5, 12]
+            },
+            {
+                name: "BlackWhite",
+                mainImage: "../img/Jordan11/BlackWhite/Jordan-11s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan11/BlackWhite/Jordan-11s-image-2.jpg",
+                    "../img/Jordan11/BlackWhite/Jordan-11s-image-3.jpg",
+                    "../img/Jordan11/BlackWhite/Jordan-11s-image-4.jpg",
+                    "../img/Jordan11/BlackWhite/Jordan-11s-image-5.jpg"
+                ],
+        sizes: [9, 9.5, 10, 11.5, 12]
+            }
+            
+        ]
+    },
+    {
+        id: 6,
+        title: "Jordan 13s",
+        price: "$375",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        colors: [
+            {
+                name: "Black/Purple",
+                mainImage: "../img/Jordan13/BlackPurple/Jordan-13s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan13/BlackPurple/Jordan-13s-image-2.jpg",
+                    "../img/Jordan13/BlackPurple/Jordan-13s-image-3.jpg",
+                    "../img/Jordan13/BlackPurple/Jordan-13s-image-4.jpg",
+                    "../img/Jordan13/BlackPurple/Jordan-13s-image-5.jpg"
+                ],
+        sizes: [9, 9.5, 10, 11.5, 12]
+            },
+            {
+                name: "Red/White",
+                mainImage: "../img/Jordan13/RedWhite/Jordan-13s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan13/RedWhite/Jordan-13s-image-2.jpg",
+                    "../img/Jordan13/RedWhite/Jordan-13s-image-3.jpg",
+                    "../img/Jordan13/RedWhite/Jordan-13s-image-4.jpg",
+                    "../img/Jordan13/RedWhite/Jordan-13s-image-5.jpg"
+                ],
+        sizes: [9, 9.5, 10, 11.5, 12]
+            },
+            {
+                name: "White/Blue/Black",
+                mainImage: "../img/Jordan13/WhiteBlueBlack/Jordan-13s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan13/WhiteBlueBlack/Jordan-13s-image-2.jpg",
+                    "../img/Jordan13/WhiteBlueBlack/Jordan-13s-image-3.jpg",
+                    "../img/Jordan13/WhiteBlueBlack/Jordan-13s-image-4.jpg",
+                    "../img/Jordan13/WhiteBlueBlack/Jordan-13s-image-5.jpg"
+                ],
+        sizes: [9, 9.5, 10, 11.5, 12]
+            }
+        ]
+    }
+]
+
+const productContainer = document.querySelector(".product-container");
+const isProductDetailPage = document.querySelector(".product-detail");
+
+if (productContainer){
+    displayProducts()
+}else if (isProductDetailPage){
+    displayProductDetail()
+}
+
+
+function displayProducts(){
+    products.forEach(product => {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+        productCard.innerHTML = `
+            <div class = "img-box">
+                <img src = "${product.colors[0].mainImage}">
+            </div>
+            <h2 class ="title"> ${product.title}</h2>
+            <span class="price">${product.price}</span>
+        `;
+        productContainer.appendChild(productCard);
+
+        const imgBox = productCard.querySelector('.img-box');
+        imgBox.addEventListener('click', ()=>{
+            sessionStorage.setItem('selectedProduct', JSON.stringify(product));
+            window.location.href ="product-detail.html"
+        })
+    });
+}
+
+
+function displayProductDetail(){
+    const productData = JSON.parse(sessionStorage.getItem('selectedProduct'));
+    
+    const titleEl = document.querySelector('.title');
+    const priceEl = document.querySelector('.price');
+    const descriptioneEl = document.querySelector('.description');
+    const mainImageContainer = document.querySelector('.main-img');
+    const thumbnailContainer = document.querySelector('.thumbnail-list');
+    const colorContainer = document.querySelector('.color-options');
+    const sizeContainer = document.querySelector('.size-options');
+    //const addToCartBtn = document.querySelector('#add-cart-btn');
+
+    let selectedColor = productData.colors[0];
+    let selectedSize = selectedColor.sizes[0];
+
+    function updateProductDisplay(colorData){
+        if(!colorData.sizes.includes(selectedSize)) {
+            selectedSize = colorData.sizes[0];
+        }
+
+        mainImageContainer.innerHTML = `<img src = "${colorData.mainImage}">`;
+        const allThumbnails = [colorData.mainImage].concat(colorData.thumbnails.slice(0, 3));
+        allThumbnails.forEach(thumb => {
+            const img = document.createElement('img');
+            img.src = thumb;
+
+            thumbnailContainer.appendChild(img)
+
+            img.addEventListener('click', ()=> {
+                mainImageContainer.innerHTML = `<img src = "${thumb}">`;
+            })
+        })
+
+        colorContainer.innerHTML = "";
+        productData.colors.forEach(color => {
+            const img = document.createElement('img');
+            img.src = color.mainImage;
+            if(color.name === colorData.name) {
+                img.classList.add('selected')
+            }
+            
+
+            colorContainer.appendChild(img)
+            img.addEventListener('click', ()=> {
+                selectedColor = color;
+                updateProductDisplay(color)
+            })
+        })
+        sizeContainer.innerHTML = "";
+        colorData.sizes.forEach(size => {
+            const btn = document.createElement('button');
+            btn.textContent = size;
+            if(size === selectedSize) {
+                btn.classList.add('selected');
+            }
+
+            sizeContainer.appendChild(btn);
+
+            btn.addEventListener('click', ()=>{
+                document.querySelectorAll('.size-options button').forEach(el => el.classList.remove('selected'));
+                btn.classList.add('selected');
+                selectedSize = size;
+            })
+        })
+    }
+    titleEl.textContent = productData.title;
+    priceEl.textContent = productData.price;
+    descriptioneEl.textContent = productData.description;
+
+    updateProductDisplay(selectedColor);
+}
---
+++ js/products.js
@@ -0,0 +1,48 @@
+const products = [
+    {
+        id: 1,
+        title: "Jordan 1s",
+        price: "$250",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        colors: [
+            {
+                name: "Black/White/Red",
+                mainImage: "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-2.jpg",
+                    "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-3.jpg",
+                    "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-4.jpg"
+                ],
+                sizes: [9, 9.5, 10, 11.5, 12, ]
+            },
+            {
+                name: "Gold/White/Black",
+                mainImage: "../img/Jordan1/GoldWhiteBlack/Jordan-1s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan1/GoldWhiteBlack/Jordan-1s-image-2.jpg",
+                    "../img/Jordan1/GoldWhiteBlack/Jordan-1s-image-3.jpg",
+                    "../img/Jordan1/GoldWhiteBlack/Jordan-1s-image-4.jpg"
+                ],
+        sizes: [9, 9.5, 10, 11.5, 12, ]
+            }     
+        ]
+    },
+    {
+        id: 2,
+        title: "Jordan 2s",
+        price: "$250",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        colors: [
+            {
+                name: "Black/White/Red",
+                mainImage: "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-1.jpg",
+                thumbnails: [
+                    "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-2.jpg",
+                    "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-3.jpg",
+                    "../img/Jordan1/BlackWhiteRed/Jordan-1s-image-4.jpg"
+                ],
+                sizes: [9, 9.5, 10, 11.5, 12, ]
+            }  
+        ]
+    }
+]
